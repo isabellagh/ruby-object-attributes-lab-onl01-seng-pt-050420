48e05d1b56cf6ff5409bc9e28a3880c8566a4c60
isabella g henriques <isabellagh88@gmail.com>
2020-05-19 12:31:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
   
-  def initialize(name) 
-    @name = name 
-  end 
-  
-  def name 
-    @name 
-  end 
+ def name = (dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
   
 end 
